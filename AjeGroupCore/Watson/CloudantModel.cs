b064077dabef1b87fee0f2971275336318fba29d
Paris Miguel Pantigoso Mosquera <proinnovaperu@outlook.com>
2017-07-25 00:38:29 -0500

    Watson files

---
+++ AjeGroupCore/Watson/CloudantModel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IBM.VCA.Watson.Watson
+{
+
+    public class CloudantModel
+    {
+        public int total_rows { get; set; }
+        public int offset { get; set; }
+        public Row[] rows { get; set; }
+    }
+
+    public class Row
+    {
+        public string id { get; set; }
+        public string key { get; set; }
+        public Value value { get; set; }
+        public Doc doc { get; set; }
+    }
+
+    public class Value
+    {
+        public string rev { get; set; }
+    }
+
+    public class Doc
+    {
+        public string _id { get; set; }
+        public string _rev { get; set; }
+        public string topic { get; set; }
+        public Payload payload { get; set; }
+        public string deviceId { get; set; }
+        public string deviceType { get; set; }
+        public string eventType { get; set; }
+        public string format { get; set; }
+    }
+
+    public class Payload
+    {
+        public D d { get; set; }
+    }
+
+    public class D
+    {
+        public string Name { get; set; }
+        public int temperatura { get; set; }
+        public int humedad { get; set; }
+    }
+
+
+
+
+    public class CloudantSelector
+    {
+        public Selector selector { get; set; }
+        public string[] fields { get; set; }
+        public Sort[] sort { get; set; }
+        public int limit { get; set; }
+    }
+
+    public class Selector
+    {
+        public _Id _id { get; set; }
+    }
+
+    public class _Id
+    {
+        public int gt { get; set; }
+    }
+
+    public class Sort
+    {
+        public string _id { get; set; }
+    }
+
+
+
+
+    public class CloudantFiltered
+    {
+        public string _id { get; set; }
+        public string _rev { get; set; }
+        public string topic { get; set; }
+        public Payload payload { get; set; }
+        public string deviceId { get; set; }
+        public string deviceType { get; set; }
+        public string eventType { get; set; }
+        public string format { get; set; }
+    }
+
+
+    public class CloudantFiltered2
+    {
+        public Doc[] docs { get; set; }
+    }
+
+
+}
---
+++ AjeGroupCore/Watson/WatsonConversationService.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using IBM.VCA.Watson.Watson.Model;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace IBM.VCA.Watson.Watson
+{
+    public class WatsonConversationService
+    {
+        public static ConversationService _conversation;
+        public static Context _ctx;
+
+        public class WatsonCredentials
+        {
+            public string workspaceID = "e5224d61-a99e-48de-bca2-fccfd032478a";
+            public string username = "a7b8ac71-3e55-49f7-bd62-ad7d905919d2";
+            public string password = "LZjxPhMiiwbQ";
+        }
+
+        public static MessageRequest Message(string text, Context ctx, WatsonCredentials creds)
+        {
+
+            MessageRequest messageRequest = new MessageRequest();
+
+            if (text == null)
+            {
+                List<Intent> myIntents = new List<Intent>();
+
+                myIntents.Add(new Intent()
+                {
+                    Confidence = 1,
+                    IntentDescription = "algomas"
+                });
+
+                messageRequest.Intents = myIntents;
+            }
+            else
+            {
+                messageRequest.Input = new InputData()
+                {
+                    Text = text
+                };
+            }
+
+
+            if (ctx != null && ctx.System != null)
+            {
+                messageRequest.Context = ctx;
+            }
+
+            _conversation = new ConversationService(creds.username, creds.password);
+
+
+            var result = _conversation.Message(creds.workspaceID, messageRequest);
+
+            if (result != null)
+            {
+                if (result.Intents.Count > 0)
+                {
+                    messageRequest.Intents = result.Intents;
+                }
+                else
+                {
+                    Console.WriteLine("Intents vacíos");
+                }
+
+                if (result.Output != null)
+                {
+                    messageRequest.Context = result.Context.ToObject<Context>();
+                    messageRequest.Output = result.Output.ToObject<OutputData>();
+
+                    return messageRequest;
+                }
+                else
+                {
+                    return messageRequest;
+                }
+            }
+            else
+            {
+
+                return messageRequest;
+            }
+        }
+
+
+        public static async Task<MessageRequest> Weather(string msg, Context ctx)
+        {
+
+            MessageRequest messageRequest = new MessageRequest()
+            {
+                Input = new InputData()
+                {
+                    Text = msg
+                }
+            };
+
+            if (ctx != null && ctx.System != null)
+            {
+                messageRequest.Context = ctx;
+            }
+
+
+
+            var result = await WeatherService.GetWeatherAsync(ctx.City, null);
+
+
+            if (result != null)
+            {
+
+                string temperature = result.main.temp.ToString();
+                string city = result.name;
+                string description = result.weather[0].description;
+                string urlIcon = string.Format("../Watson/Content/weather/{0}.png", result.weather[0].icon);
+
+                messageRequest.Context.City = city;
+                messageRequest.Context.Temperature = temperature;
+                messageRequest.Context.Description = description;
+
+                string forecast =
+                    "<div id='weather_widget' class='weather_widget'>" +
+                    "<div id= 'weather_widget_city_name' class='weather_widget_city_name'>Clima de " + city + "</div>" +
+                    "<h3 id= 'weather_widget_temperature' class='weather_widget_temperature'>" +
+                    "<img src='" + urlIcon + "'> " + temperature + "°C</h3>" +
+                    "<div id='weather_widget_main' class='weather_widget_main'>" + description + "</div>";
+
+                List<string> list = new List<string>();
+
+                list.Add(forecast);
+
+
+                OutputData output = new OutputData
+                {
+                    Text = list
+                };
+
+
+
+                messageRequest.Output = output;
+
+            }
+
+            return messageRequest;
+        }
+
+    }
+}
